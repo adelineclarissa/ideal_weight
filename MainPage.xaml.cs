148fd3e5bd5485e3d4d22989ac1e926aea16a629
Adeline C. Trisnobuwono <84635235+adelineclarissa@users.noreply.github.com>
2023-04-06 14:28:37 -0700

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Ideal_Weight;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void calculatorClicked(object sender, EventArgs e)
+	{
+		// navigate to calculator page
+		await Navigation.PushAsync(new CalculatorView());
+	}
+
+    private void apiClicked(object sender, EventArgs e)
+    {
+        // navigate to calculator page
+    }
+
+    void btn2_Clicked(System.Object sender, System.EventArgs e)
+    {
+    }
+}
+
+
