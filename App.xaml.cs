ecfadb038347b25c191582f38a0fcbd135da0c1f
Adeline C. Trisnobuwono <84635235+adelineclarissa@users.noreply.github.com>
2023-04-06 14:27:43 -0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ideal_Weight;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+        MainPage = new NavigationPage(new MainPage());
+    }
+}
+
